0f886ec3af59d23004db85ae4f84d51b87744f5b
saiteja-code <saitejakanaparthy@gmail.com>
2020-11-05 23:29:12 +0530

    multiple contacts unique name

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,76 +1,103 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Address objmain = new Address();
-        int n = 0;
-        while (n != 5) {
-            System.out.println("1.Add\n2.Edit\n3.Delete\n4.View\n5.Exit");
-            System.out.println("Enter your choice: ");
-            n = sc.nextInt();
-            if (n == 1) {
-                Contact obj = new Contact();
-                System.out.println("Add a contact: ");
-                System.out.println("Enter the First Name: ");
-                sc.nextLine();
-                obj.setFirstName(sc.nextLine());
-                System.out.println("Enter the Last Name: ");
-                obj.setLastName(sc.nextLine());
-                System.out.println("Enter Address");
-                obj.setAddress(sc.nextLine());
-                System.out.println("Enter city");
-                obj.setCity(sc.nextLine());
-                System.out.println("Enter state");
-                obj.setState(sc.nextLine());
-                System.out.println("Enter zip code");
-                obj.setZip(Long.parseLong(sc.nextLine()));
-                System.out.println("Enter the Phone No.: ");
-                obj.setPhoneNumber(Long.parseLong(sc.nextLine()));
-                System.out.println("Enter the Email: ");
-                obj.setEmailId(sc.nextLine());
-                objmain.addContact(obj);
+        HashMap<String, Address> hashMap = new HashMap<String, Address>();
+        System.out.println("Welcome to address book program");
+        int z = 0;
+        while (z == 0) {
+            System.out.println("1.Create Address Book\n2.Add Details to Address Book\n3.View contact by city \n4.Exit");
+            int k = sc.nextInt();
+            if (k == 1) {
+                System.out.println("Enter the name of address book to be created");
+                String name = sc.next();
+                Address obj = new Address();
+                hashMap.put(name, obj);
             }
-            if (n == 2) {
-                System.out.println("Edit a contact: ");
-                System.out.println("Enter the First Name: ");
-                sc.nextLine();
-                String firstName = sc.nextLine();
-                System.out.println("Enter the Last Name: ");
-                String lastName = sc.nextLine();
-                boolean f2 = objmain.editContactGivenName(firstName, lastName);
-                if (f2)
-                    System.out.println("The contact is successfully Edited");
-                else
-                    System.out.println("Contact is not found");
+            if (k == 2) {
+                Contact obj1 = new Contact();
+                System.out.println("Enter the name of the address book to be accesed");
+                String name = sc.next();
+                if (hashMap.containsKey(name)) {
+                    int n = 0;
+                    Address objmain = new Address();
+                    while (n != 5) {
+                        System.out.println(
+                                "Menu\n1.Add\n2.Edit Contact\n3.Delete Contact\n4.Display all contacts\n5.Exit");
+                        System.out.println("Enter your choice: ");
+                        n = sc.nextInt();
+                        if (n == 1) {
+                            Contact obj = new Contact();
+                            System.out.println("Add a contact: ");
+                            System.out.println("Enter the First Name: ");
+                            sc.nextLine();
+                            obj.setFirstName(sc.nextLine());
+                            System.out.println("Enter the Last Name: ");
+                            obj.setLastName(sc.nextLine());
+                            System.out.println("Enter Address");
+                            obj.setAddress(sc.nextLine());
+                            System.out.println("Enter city");
+                            obj.setCity(sc.nextLine());
+                            System.out.println("Enter state");
+                            obj.setState(sc.nextLine());
+                            System.out.println("Enter zip code");
+                            obj.setZip(Long.parseLong(sc.nextLine()));
+                            System.out.println("Enter the Phone No.: ");
+                            obj.setPhoneNumber(Long.parseLong(sc.nextLine()));
+                            System.out.println("Enter the Email: ");
+                            obj.setEmailId(sc.nextLine());
+                            hashMap.get(name).addContact(obj);
+                        }
+                        if (n == 2) {
+                            System.out.println("Edit a contact: ");
+                            System.out.println("Enter the First Name: ");
+                            sc.nextLine();
+                            String firstName = sc.nextLine();
+                            System.out.println("Enter the Last Name: ");
+                            String lastName = sc.nextLine();
+                            boolean f2 = hashMap.get(name).editContactGivenName(firstName, lastName);
+                            if (f2)
+                                System.out.println("The contact is successfully Edited");
+                            else
+                                System.out.println("Contact is not found");
 
-            }if (n == 3) {
-                System.out.println("Delete a contact :");
-                System.out.println("Enter the First Name: ");
-                sc.nextLine();
-                String firstName = sc.nextLine();
-                System.out.println("Enter the Last Name: ");
-                String lastName = sc.nextLine();
-                boolean f1 = objmain.removeContact(firstName, lastName);
-                if (f1)
-                    System.out.println("The contact is successfully deleted");
-                else
-                    System.out.println("Contact is not found");
+                        }
+                        if (n == 3) {
+                            System.out.println("Delete a contact :");
+                            System.out.println("Enter the First Name: ");
+                            sc.nextLine();
+                            String firstName = sc.nextLine();
+                            System.out.println("Enter the Last Name: ");
+                            String lastName = sc.nextLine();
+                            boolean f1 = hashMap.get(name).removeContact(firstName, lastName);
 
-            }
-            if (n == 4) {
-                System.out.println("The contacts in the List are:");
-                List<Contact> list = objmain.viewAllContacts();
-                for (Contact obj : list) {
-                    System.out.println("First Name : " + obj.getFirstName());
-                    System.out.println("Last Name : " + obj.getLastName());
-                    System.out.println("Address : " + obj.getAddress());
-                    System.out.println("City : " + obj.getCity());
-                    System.out.println("State : " + obj.getState());
-                    System.out.println("Zip Code : " + obj.getZip());
-                    System.out.println("Phone number : " + obj.getPhoneNumber());
-                    System.out.println("Email :" + obj.getEmailId());
+                            if (f1)
+                                System.out.println("The contact is successfully deleted");
+                            else
+                                System.out.println("Contact is not found");
+
+                        }
+
+                        if (n == 4) {
+                            System.out.println("The contacts in the List are:");
+                            List<Contact> list = hashMap.get(name).viewAllContacts();
+                            for (Contact obj : list) {
+                                System.out.println("First Name : " + obj.getFirstName());
+                                System.out.println("Last Name : " + obj.getLastName());
+                                System.out.println("Address : " + obj.getAddress());
+                                System.out.println("City : " + obj.getCity());
+                                System.out.println("State : " + obj.getState());
+                                System.out.println("Zip Code : " + obj.getZip());
+                                System.out.println("Phone number : " + obj.getPhoneNumber());
+                                System.out.println("Email :" + obj.getEmailId());
+                            }
+                        }
+                    }
+                } else {
+                    System.out.println("No Address book found");
                 }
             }
         }
