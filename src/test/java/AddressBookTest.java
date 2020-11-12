a85d1c180d38c70465110e5de9386621329a6728
saiteja-code <saitejakanaparthy@gmail.com>
2020-11-12 16:52:22 +0530

    json server

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,42 +1,68 @@
+import com.google.gson.Gson;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import static com.cg.addressbook.AddressBookSystem.IOService.DB_IO;
+import static com.cg.addressbook.AddressBookSystem.IOService.REST_IO;
+
+
 public class AddressBookTest {
 
     @Test
-    public void MatchContactCount() {
+    public void givenAddressBookContactInDB_WhenRetrieved_ShouldMatchContactsCount() {
         AddressBookSystem addressBookSystem = new AddressBookSystem();
-        List<Contact> addressBookContactList = addressBookSystem.readAddressBookData( AddressBookSystem.IOService.DB_IO );
+        List<Contact> addressBookContactList = addressBookSystem.readAddressBookData( DB_IO );
         System.out.println(addressBookContactList);
-        Assert.assertEquals(7,addressBookContactList.size());
+        Assert.assertEquals(5,addressBookContactList.size());
     }
 
     @Test
-    public void givenNewZipForContactSyncWithDataBase() {
+    public void givenNewZipForParticularContact_WhenUpdated_ShouldSyncWithDataBase() {
         AddressBookSystem addressBookSystem = new AddressBookSystem();
-        List<Contact> employeePayrollData = addressBookSystem.readAddressBookData(AddressBookSystem.IOService.DB_IO);
-        addressBookSystem.updateContactPhoneNo("Sai",503692);
-        boolean result = addressBookSystem.checkAddressBookInsyncWithDB("Sai");
+        List<Contact> employeePayrollData = addressBookSystem.readAddressBookData(DB_IO);
+        addressBookSystem.updateContactPhoneNo("Akhil",Long.parseLong( "74112"));
+        boolean result = addressBookSystem.checkAddressBookInsyncWithDB("Akhil");
         Assert.assertTrue( result );
     }
+
     @Test
-    public void givenAddressBookContactData_WhenRetrieveByCity_ShouldReturnProperValue() {
+    public void givenDateRange_WhenRetrieved_ShouldMatchAddressBookContactsCount() {
+        AddressBookSystem addressBookSystem = new AddressBookSystem();
+        addressBookSystem.readAddressBookData(DB_IO);
+        LocalDate startDate = LocalDate.of( 2018,01,01 );
+        LocalDate endDate = LocalDate.now();
+        List<Contact> addressBookContactData = addressBookSystem.readAddressBookContactDataForDateRange(DB_IO,startDate,endDate );
+        Assert.assertEquals( 3,addressBookContactData.size() );
+    }
+
+    @Test
+    public void givenAddressBookContacts_WhenCountByCityRetrieve_ShouldReturnProperValue() {
         AddressBookSystem addressBookSystem = new AddressBookSystem();
         addressBookSystem.readAddressBookData(DB_IO);
         Map<String,Integer> countContactByCity = addressBookSystem.readContactCountByCity(DB_IO);
         Assert.assertTrue( countContactByCity.get("Mahabubnagar").equals( 2 )
                 && countContactByCity.get( "Hosur" ).equals( 1 ) && countContactByCity.get( "Kurnool" ).equals( 1 ) && countContactByCity.get( "Kadapa" ).equals( 1 ));
     }
+
     @Test
     public void givenNewContact_WhenAdded_ShouldSyncWithDB() {
         AddressBookSystem addressBookSystem = new AddressBookSystem();
-        addressBookSystem.readAddressBookData(DB_IO);
-        addressBookSystem.addContactToAddressBook("ab","Bunny","Balajinagar","vizag","AP",Long.parseLong( "92512"),Long.parseLong( "9869854127" ),"xyz@gmail.com","family",LocalDate.now());
-        boolean result = addressBookSystem.checkAddressBookInsyncWithDB("ab");
-        Assert.assertTrue( result );
+        List<Contact> addressBookContactList = addressBookSystem.readAddressBookData( DB_IO );
+        addressBookSystem.addContactToAddressBook("ab","Bunny","Balajinagar","vizag",
+                "AP",Long.parseLong( "92512"),Long.parseLong( "9869854127" ),"xyz@gmail.com","family",LocalDate.now());
+        Assert.assertEquals( 6, addressBookContactList.size());
     }
+
     @Test
     public void given6Contact_WhenAddedToDB_ShouldMatchContactEntries() {
         Contact[] arrayOfContacts = {
@@ -62,9 +88,10 @@ public class AddressBookTest {
         Instant threadStart = Instant.now();
         addressBookSystem.addContactsToAddressBookWithThreads(Arrays.asList(arrayOfContacts));
         Instant threadend = Instant.now();
-        System.out.println("Duration with thread: "+ Duration.between( threadStart,threadend ));
-        Assert.assertEquals( 17,addressBookSystem.countEntries(DB_IO) );
+        System.out.println("Duration with thread: "+Duration.between( threadStart,threadend ));
+        Assert.assertEquals( 42,addressBookSystem.countEntries(DB_IO) );
     }
+
     @Before
     public void setUp()  {
         RestAssured.baseURI = "http://localhost";
@@ -95,4 +122,21 @@ public class AddressBookTest {
         Assert.assertEquals( 2,entries);
     }
 
+    @Test
+    public void givenNewContact_WhenAdded_ShouldMatch201ResponseAndCount(){
+        AddressBookSystem addressBookSystem;
+        Contact[] arrayOfEmps = getContactList();
+        addressBookSystem = new AddressBookSystem( Arrays.asList(arrayOfEmps));
+        Contact contact = null;
+        contact = new Contact( "vijay","kanth","padmavathicolony","Manchiriyal","Tamilnadu",
+                Long.parseLong( "7654"),Long.parseLong( "8799854172" ),"xcx@gmail.com","family",LocalDate.now());
+        Response response = addContactToJsonServer( contact );
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals( 201,statusCode );
+
+        contact = new Gson().fromJson(response.asString(),Contact.class);
+        addressBookSystem.addContactToAddressBook(contact,REST_IO);
+        long entries = addressBookSystem.countEntries( REST_IO );
+        Assert.assertEquals( 3,entries);
+    }
 }
